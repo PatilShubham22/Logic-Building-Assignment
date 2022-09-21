826312b396b07adda433abf8d715a2db26be1d69
PatilShubham22 <shubham22398patil@gmail.com>
2022-09-21 09:09:27 +0530

    Added New File

---
+++ Assignment 06/1. Accept name from user & print that name.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    char Name[30];
+
+    printf("\n Please enter full name = ");
+    gets(Name);
+
+    printf("\n******************************");
+    printf("\n\n Your name is %s",Name);
+    printf("\n\n******************************");
+
+    return 0;
+}
