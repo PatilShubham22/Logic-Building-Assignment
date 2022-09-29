c8c720757cf58696c7f228ac0be7aeeac6e078bc
PatilShubham22 <shubham22398patil@gmail.com>
2022-09-29 20:10:08 +0530

    New Program Added

---
+++ Assignment 09/4. Find odd factorial of given number.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int Odd_Factor(int iNo)
+{
+    int Fact = 0;
+
+    for(Fact = 1; iNo > 0; iNo--)
+    {
+        if(iNo % 2 != 0)
+        {
+            Fact = Fact * iNo;
+        }
+    }
+    if(iNo < 0)
+    {
+        for(Fact = 1; iNo < 0; iNo++)
+        {
+           if(iNo % 2 != 0)
+           {
+              Fact = Fact * iNo;
+           }
+        }
+    }
+
+  return Fact;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("\n Enter a Number = ");
+    scanf("%d",&iValue);
+
+    iRet = Odd_Factor(iValue);
+
+    printf("\n***************************************************");
+    printf("\n\n Odd factorial of number %d = %d",iValue ,iRet);
+    printf("\n\n***************************************************");
+  return 0;
+}
