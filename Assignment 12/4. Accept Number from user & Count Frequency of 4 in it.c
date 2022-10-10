3e2df99ae15672b03f972556efcfd06c2764ca72
PatilShubham22 <shubham22398patil@gmail.com>
2022-10-10 09:33:13 +0530

    Added New File

---
+++ Assignment 12/4. Accept Number from user & Count Frequency of 4 in it.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int Count_Two(int iNo)
+{
+    int Cnt = 0, Digit = 0;
+
+    while(iNo > 0)
+    {
+        Digit = iNo % 10;
+        iNo = iNo / 10;
+
+        if(Digit == 4)
+        {
+            Cnt++;
+        }
+    }
+    return Cnt;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter Number = ");
+    scanf("%d",&iValue);
+
+    iRet = Count_Two(iValue);
+
+    printf("\n %d",iRet);
+
+  return 0;
+}
