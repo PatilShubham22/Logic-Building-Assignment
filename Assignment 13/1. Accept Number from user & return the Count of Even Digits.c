fb995c2b19aa24619d96e55c3022e9eebcb136df
PatilShubham22 <shubham22398patil@gmail.com>
2022-10-12 08:30:31 +0530

    Added New File

---
+++ Assignment 13/1. Accept Number from user & return the Count of Even Digits.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int Count_Even(int iNo)
+{
+    int Cnt = 0,Digit = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo > 0)
+    {
+        Digit = iNo % 10;
+        iNo = iNo / 10;
+        if(Digit % 2 == 0)
+        {
+            Cnt++;
+        }
+    }
+    return Cnt;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter a Number = ");
+    scanf("%d",&iValue);
+
+    iRet = Count_Even(iValue);
+
+    printf("\n %d",iRet);
+
+   return 0;
+}
