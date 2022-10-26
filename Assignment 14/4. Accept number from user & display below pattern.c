49c8828b25490cf4af10d08cb04e89b8ebea83be
PatilShubham22 <shubham22398patil@gmail.com>
2022-10-26 09:01:48 +0530

    Added Pattern Assignment

---
+++ Assignment 14/4. Accept number from user & display below pattern.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int i = 0;
+
+    for(i = 1; i <= iNo; i++)
+    {
+        printf(" # ");
+        printf(" %d ",i);
+        printf(" * ");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("\n Enter number of elements = ");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+  return 0;
+}
