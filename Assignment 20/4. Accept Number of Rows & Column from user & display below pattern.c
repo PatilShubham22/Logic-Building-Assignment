50dbe575c67a48d26f8f76b06f37e2b477dda3b0
PatilShubham22 <shubham22398patil@gmail.com>
2022-12-11 11:20:54 +0530

    Added New Program

---
+++ Assignment 20/4. Accept Number of Rows & Column from user & display below pattern.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iColumn)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iColumn; j++)
+        {
+            if(i + j == (iRow + 1) || i == 1 || j == 1 || i == iRow || j == iColumn)
+            {
+                printf(" * ");
+            }
+            else if(i + j <= iRow)
+            {
+                printf(" # ");
+            }
+            else
+            {
+                printf(" @ ");
+            }
+        }
+       printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("\n Enter Row & Column Number = ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+  return 0;
+}
+
+
